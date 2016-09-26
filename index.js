b8d33cd34f740396edf06a1c38dd1922cad36c6e
Schyler Hosler <schylerhosler@gmail.com>
2016-09-25 22:33:11 -0400

    Initial commit: React.js introduction to concept of controlled vs uncontrolled components in terms of forms

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 const app = "I don't do much.";
+
+class ControlledInput extends React.Component {
+  constructor() {
+    super();
+
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state = {
+      value: '',
+    };
+  }
+
+  handleChange(ev) {
+    this.setState({
+      value: ev.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <input type="text" value={this.state.value} onChange={this.handleChange} />
+    );
+  }
+}
